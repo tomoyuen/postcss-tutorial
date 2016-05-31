a0cc68f2a7582711d259f2abdb3d6dd3c12a736f
tomoyuen <yuen@tomo.edu.tf>
2016-06-01 02:42:49 +0800

    'change-fonts

--- app/index.js
+++ app/index.js
@@ -1,3 +1,34 @@
 require('normalize-css');
 require('./index.css');
-require('font-awesome-webpack');
+import $ from 'jquery';
+
+$(function() {
+	$('.category li').on('click', function() {
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+		if ($(this).is('.no-sub')) {
+			$('.sub-category').hide();
+		} else {
+			$('.sub-category').show();
+		}
+	});
+
+	$('.sub-category li').on('click', function() {
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('.sidebar-cartbtn').on('click', function() {
+		$(this).addClass('active');
+		$('.sidebar').animate({
+			right: '295px'
+		}, 300);
+	});
+
+	$('.sidebarbd-caption .icon-angle-double-right').on('click', function() {
+		$('.sidebarbd-cartbtn').removeClass('active');
+		$('.sidebar').animate({
+			right: '0'
+		}, 300);
+	})
+})
